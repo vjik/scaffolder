8b1088399dda5039d7503f0202b332313af7e190
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-01-04 11:58:30 +0300

    Refactor ChangeIf to support multiple changes

--- src/Change/ChangeIf.php
+++ src/Change/ChangeIf.php
@@ -8,20 +8,46 @@ use Vjik\Scaffolder\Change;
 use Vjik\Scaffolder\Context;
 use Vjik\Scaffolder\Fact;
 
+use function is_array;
+use function is_callable;
+
 final readonly class ChangeIf implements Change
 {
     /**
+     * @var list<Change>
+     */
+    private array $changes;
+
+    /**
+     * @param Change|list<Change> $change
      * @param class-string<Fact<bool>> $fact
      */
     public function __construct(
-        private Change $change,
+        Change|array $change,
         private string $fact,
-    ) {}
+    ) {
+        $this->changes = is_array($change) ? $change : [$change];
+    }
 
     public function decide(Context $context): callable|array|null
     {
-        return $context->getFact($this->fact)
-            ? $this->change->decide($context)
-            : null;
+        if (!$context->getFact($this->fact)) {
+            return null;
+        }
+
+        $appliers = [];
+        foreach ($this->changes as $change) {
+            $applier = $change->decide($context);
+            if ($applier === null) {
+                continue;
+            }
+            if (is_callable($applier)) {
+                $appliers[] = $applier;
+            } else {
+                $appliers = array_merge($appliers, $applier);
+            }
+        }
+
+        return $appliers === [] ? null : $appliers;
     }
 }
