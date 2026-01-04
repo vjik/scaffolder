899f01b6db77ebaa069076c92de155a7b3383352
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-01-04 13:08:33 +0300

    Refactor EnsureDirectoryWithGitkeep to support Fact-based directory handling

--- src/Change/EnsureDirectoryWithGitkeep.php
+++ src/Change/EnsureDirectoryWithGitkeep.php
@@ -7,29 +7,38 @@ namespace Vjik\Scaffolder\Change;
 use Vjik\Scaffolder\Change;
 use Vjik\Scaffolder\Cli;
 use Vjik\Scaffolder\Context;
+use Vjik\Scaffolder\Fact;
 
 use function sprintf;
 
 final readonly class EnsureDirectoryWithGitkeep implements Change
 {
+    /**
+     * @param string|class-string<Fact<string>> $directory
+     */
     public function __construct(
         private string $directory,
     ) {}
 
     public function decide(Context $context): ?callable
     {
-        if ($context->fileExists($this->directory)
+        /** @var string $directory */
+        $directory = is_a($this->directory, Fact::class, true)
+            ? $context->getFact($this->directory)
+            : $this->directory;
+
+        if ($context->fileExists($directory)
             && (
-                !$context->isDirectory($this->directory)
-                || !$context->isDirectoryEmpty($this->directory)
+                !$context->isDirectory($directory)
+                || !$context->isDirectoryEmpty($directory)
             )
         ) {
             return null;
         }
 
-        return fn(Cli $cli) => $cli->step(
-            sprintf('Create `.gitkeep` in `%s`', $this->directory),
-            fn() => $context->writeFile($this->directory . '/.gitkeep'),
+        return static fn(Cli $cli) => $cli->step(
+            sprintf('Create `.gitkeep` in `%s`', $directory),
+            static fn() => $context->writeTextFile($directory . '/.gitkeep'),
         );
     }
 }
