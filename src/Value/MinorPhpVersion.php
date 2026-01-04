ef71de96e8f8ef63ace4929edb0c1b4706494a19
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-01-04 15:00:24 +0300

    Add range method to MinorPhpVersion enum for version filtering

--- src/Value/MinorPhpVersion.php
+++ src/Value/MinorPhpVersion.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Vjik\Scaffolder\Value;
 
+use function array_slice;
+
 enum MinorPhpVersion: string
 {
     case PHP74 = '7.4';
@@ -14,4 +16,24 @@ enum MinorPhpVersion: string
     case PHP84 = '8.4';
     case PHP85 = '8.5';
     case UNKNOWN = 'unknown';
+
+    /**
+     * @return list<self>
+     */
+    public static function range(self $from, self $to): array
+    {
+        $versions = array_filter(
+            self::cases(),
+            static fn(self $version): bool => $version !== self::UNKNOWN,
+        );
+
+        $fromIndex = array_search($from, $versions, true);
+        $toIndex = array_search($to, $versions, true);
+
+        if ($fromIndex === false || $toIndex === false || $fromIndex > $toIndex) {
+            return [];
+        }
+
+        return array_slice($versions, $fromIndex, $toIndex - $fromIndex + 1);
+    }
 }
