775240a405e3fd46bb9ee38f4c567cfdf122e208
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-05-20 16:33:23 +0300

    Add `package-authors-default-source` setting to `PackageAuthors` fact (#4)

--- src/Fact/PackageAuthors.php
+++ src/Fact/PackageAuthors.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace Vjik\Scaffolder\Fact;
 
+use InvalidArgumentException;
 use Vjik\Scaffolder\Cli;
 use Vjik\Scaffolder\Context;
 use Vjik\Scaffolder\Fact;
 use Vjik\Scaffolder\Value\PackageAuthor;
+
+use function array_map;
+use function get_debug_type;
+use function implode;
+use function is_string;
+use function sprintf;
 
 /**
  * @extends Fact<list<PackageAuthor>>
@@ -15,6 +22,7 @@ use Vjik\Scaffolder\Value\PackageAuthor;
 final class PackageAuthors extends Fact
 {
     public const string VALUE_OPTION = 'package-authors';
+    public const string DEFAULT_SOURCE_OPTION = 'package-authors-default-source';
 
     public static function resolve(Cli $cli, Context $context): mixed
     {
@@ -32,10 +40,50 @@ final class PackageAuthors extends Fact
             );
         }
 
-        return [
-            new PackageAuthor(
-                name: $context->getFact(UserName::class),
-                email: $context->getFact(UserEmail::class),
-            )];
+        return match (self::resolveDefaultSource($context)) {
+            PackageAuthorsDefaultSource::User => [
+                new PackageAuthor(
+                    name: $context->getFact(UserName::class),
+                    email: $context->getFact(UserEmail::class),
+                ),
+            ],
+            PackageAuthorsDefaultSource::EmptyList => [],
+        };
+    }
+
+    private static function resolveDefaultSource(Context $context): PackageAuthorsDefaultSource
+    {
+        $source = $context->getParam(self::DEFAULT_SOURCE_OPTION, PackageAuthorsDefaultSource::User);
+
+        if ($source instanceof PackageAuthorsDefaultSource) {
+            return $source;
+        }
+
+        if (is_string($source)) {
+            $result = PackageAuthorsDefaultSource::tryFrom($source);
+            if ($result === null) {
+                throw new InvalidArgumentException(
+                    sprintf(
+                        'Invalid value "%s" for param "%s". Allowed values: "%s".',
+                        $source,
+                        self::DEFAULT_SOURCE_OPTION,
+                        implode('", "', array_map(
+                            static fn(PackageAuthorsDefaultSource $case): string => $case->value,
+                            PackageAuthorsDefaultSource::cases(),
+                        )),
+                    ),
+                );
+            }
+            return $result;
+        }
+
+        throw new InvalidArgumentException(
+            sprintf(
+                'Param "%s" must be a string or %s, %s given.',
+                self::DEFAULT_SOURCE_OPTION,
+                PackageAuthorsDefaultSource::class,
+                get_debug_type($source),
+            ),
+        );
     }
 }
